ab140d73558cf7614c24fe1d12e086e6d32e421c
MilkAlien <ndrgrt@ya.ru>
2021-01-08 20:56:16 +0300

    Update unpack-img.sh

--- unpack-img.sh
+++ unpack-img.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo=0
 mv *.img gpon.img
-s01=$(binwalk gpon.img | awk '/gzip/ {print $1;}')
-s02=$(binwalk gpon.img | awk '/gzip/ {print $2;}' | awk -F"x" '{print $2;}')
-head -c$s01 gpon.img > header3
+# s01=$(binwalk gpon.img | awk '/gzip/ {print $1;}')
+# s02=$(binwalk gpon.img | awk '/gzip/ {print $2;}' | awk -F"x" '{print $2;}')
+# head -c$s01 gpon.img > header3
 binwalk -Me gpon.img
-cd _*
+# cd _*
 # s03=$(binwalk $s02 | awk '/LZMA/ {print $1;}')
 # s04=$(binwalk $s02 | awk '/LZMA/ {print $2;}' | awk -F"x" '{print $2;}')
-head -c608 $s02  > ../header2
-cd _*
-mv squashfs-root ../../squashfs-root
-cd ../../squashfs-root
+# head -c608 $s02  > ../header2
+# cd _*
+mv _*/_*/squashfs-root ./squashfs-root
+cd squashfs-root
 rm -rf ../_*
